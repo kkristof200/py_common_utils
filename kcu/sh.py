aa2cfd4021b12524472fa755f7fb14f3d5c02a69
Kristof <kristof.kovacs@telenav.com>
2020-06-26 00:54:48 +0300

    new version

--- kcu/sh.py
+++ kcu/sh.py
@@ -11,3 +11,15 @@ def sh(
         print(cmd)
 
     return subprocess.getoutput(cmd)
+
+def pwd() -> str:
+    return sh('pwd')
+
+def mkdir(p: str) -> str:
+    return sh('mkdir ' + path(p))
+
+def rmrf(p: str) -> str:
+    return sh('rm -rf ' + path(p))
+
+def cp(from_path: str, to_path: str) -> str:
+    return sh('cp ' + path(from_path) + ' ' + path(to_path))
