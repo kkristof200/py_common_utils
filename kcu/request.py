4f2e3cd7dd5e703aafb4ac8c44c0d2be1759f745
kkristof200 <kovacskristof200@gmail.com>
2020-11-01 18:33:17 +0200

    new version

--- kcu/request.py
+++ kcu/request.py
@@ -294,7 +294,7 @@ def __request(
 
     proxies = None
 
-    if pproxy, roxy_http or proxy_https or proxy_ftp:
+    if proxy, proxy_http or proxy_https or proxy_ftp:
         proxies = {}
 
         if proxy_http:
