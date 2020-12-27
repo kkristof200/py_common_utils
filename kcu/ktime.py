7e0cc40cd7df703d4ce927ea20e91779b792eaab
kkristof200 <kovacskristof200@gmail.com>
2020-12-27 19:38:33 +0200

    new version

--- kcu/ktime.py
+++ kcu/ktime.py
@@ -1,3 +1,39 @@
+def seconds_to_time_str(seconds: float) -> str:
+    hours = int(seconds/3600)
+    seconds -= hours*3600
+
+    minutes = int(seconds/60)
+    seconds -= minutes*60
+
+    millis = seconds - int(seconds)
+    seconds = int(seconds)
+    time_str = ''
+
+    if hours > 0:
+        time_str = str(hours).zfill(2)
+
+    if minutes > 0 or len(time_str) > 0:
+        if len(time_str) > 0:
+            time_str += ':'
+
+        time_str += str(minutes).zfill(2)
+
+    if seconds > 0 or len(time_str) > 0:
+        if len(time_str) > 0:
+            time_str += ':'
+
+        time_str += str(seconds).zfill(2)
+
+    if millis > 0:
+        time_str += '.'
+
+        time_str += str(int(millis*1000)).rstrip('0')
+
+    return time_str
+
+def time_str_to_seconds(time_str: str) -> float:
+    return sum([float(c) * pow(60, i) for i, c in enumerate(reversed(time_str.split(':')))])
+
 def is_between_hours_utc(start_h_utc: float, stop_h_utc: float) -> bool:
     return is_between_seconds_utc(start_h_utc*3600, stop_h_utc*3600)
 
