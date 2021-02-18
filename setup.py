cda8f6fb320d9edbc8b941803036c71bd8e62e38
kkristof200 <kovacskristof200@gmail.com>
2021-02-18 20:04:53 +0200

    Update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="kcu",
-    version="0.0.62",
+    version="0.0.63",
     author="Kristof",
     description="some python utility methods that I often use compiled in a package",
     long_description=long_description,
